10102343a03adea3fd7781bb5f5e5deebe624ebe
AshnaB7 <77634237+AshnaB7@users.noreply.github.com>
2022-04-12 10:18:42 +0530

    Create bs.py

---
+++ bs.py
@@ -0,0 +1,12 @@
+import time
+import getpass
+from tqdm.auto import tqdm
+import sqlite3
+import pyzbar.pyzbar as pyzbar
+import pyqrcode
+import cv2
+import os
+import numpy
+import colorama
+from colorama import Back, Style
+colorama.init(autoreset=True)
