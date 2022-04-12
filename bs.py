bb5321aa94c9fe0aa90afb403a3a3030bb7026de
AshnaB7 <77634237+AshnaB7@users.noreply.github.com>
2022-04-12 10:20:04 +0530

    Update bs.py

--- bs.py
+++ bs.py
@@ -10,3 +10,27 @@ import numpy
 import colorama
 from colorama import Back, Style
 colorama.init(autoreset=True)
+
+
+#------CreateDatabaseForeEmployee------------------
+def database():
+	conn = sqlite3.connect('EmployeeDatabase.db')
+	c = conn.cursor()
+	c.execute("CREATE TABLE IF NOT EXISTS all_record(employee_name TEXT, employee_id TEXT, employee_contact, employee_department TEXT)")
+	conn.commit()
+	conn.close()
+database()
+
+
+#-------MainPage----------------------------
+def markattendance():
+	print("+------------------------------+")
+	print("|  1- Mark Attendance          |")
+	print("|  2- Admin Login              |")
+	print("+------------------------------+")
+	user_input2 = input("")
+	if user_input2== '1':
+		scan()
+	if user_input2 == '2':
+		login()
+markattendance()
